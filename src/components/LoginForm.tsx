e953c90658fd7c72429b14d9bba60f2d41a19ab3
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-22 12:30:25 +0200

    feat login form validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useRef } from "react";
 import {
   Flex,
   Heading,
@@ -10,16 +9,14 @@ import {
   useColorMode,
   useColorModeValue,
   Image,
+  VStack,
+  FormErrorMessage
 } from "@chakra-ui/react";
-import gsap from "gsap";
 import { useNavigate } from "react-router-dom";
+import { Formik, Field } from "formik";
 
 const LoginForm = () => {
   const { toggleColorMode } = useColorMode();
-  const rickPortalRef = useRef(null);
-  const mortyPortalRef = useRef(null);
-  const rickRef = useRef(null);
-  const mortyRef = useRef(null);
 
   const formBackground = useColorModeValue("gray.100", "gray.700");
   const navigate = useNavigate();
@@ -27,129 +24,68 @@ const LoginForm = () => {
     e.preventDefault();
     navigate("/characters");
   };
-  useEffect(() => {
-    const rickTimeLine = gsap.timeline();
-    const mortyTimeLine = gsap.timeline();
 
-    rickTimeLine
-      .fromTo(
-        rickPortalRef.current,
-        { transform: "Scale(0)", opacity: 0 },
-        { transform: "Scale(1)", opacity: 1, duration: 3, ease: "back" }
-      )
-      .fromTo(
-        rickRef.current,
-        { opacity: 0 },
-        { opacity: 1, duration: 1 },
-        "-=0.8"
-      )
-      .to(rickPortalRef.current, {
-        transform: "Scale(0)",
-        opacity: 1,
-        duration: 2,
-      });
-
-    mortyTimeLine
-      .fromTo(
-        mortyPortalRef.current,
-        { transform: "Scale(0)", opacity: 0 },
-        { transform: "Scale(1)", opacity: 1, duration: 3, ease: "back" }
-      )
-      .fromTo(
-        mortyRef.current,
-        { opacity: 0 },
-        { opacity: 1, duration: 1 },
-        "-=0.8"
-      )
-      .to(mortyPortalRef.current, {
-        transform: "Scale(0)",
-        opacity: 1,
-        duration: 2,
-      });
-  }, []);
   return (
-    <Flex
-      height="100vh"
-      w="100vw"
-      bg="customGreen.200"
-      alignItems="center"
-      justifyContent="center"
+    <Formik
+      initialValues={{ username: "" }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
     >
-      <Flex
-        flexDirection="column"
-        // bg={formBackground}
-        p={{ base: 6, md: 12 }}
-        borderRadius={8}
-        border="2px"
-        borderColor= "customGreen.50"
-        boxShadow="lg"
-        position="relative"
-        bg="customGreen.100"
-      >
-        <Image
-          src="./Rick-and-Morty.png"
-          position="absolute"
-          top={-24}
-          right={0}
-        />
+      {({ handleSubmit, errors, touched }) => (
+        <form onSubmit={handleSubmit}>
+          <VStack
+            spacing={4}
+            p={{ base: 6, md: 12 }}
+            borderRadius={8}
+            border="2px"
+            borderColor="customGreen.50"
+            boxShadow="lg"
+            position="relative"
+            bg="customGreen.100"
+          >
+            <Heading mb={6} color="customGreen.50">
+              Log In
+            </Heading>
+            <FormControl isInvalid={!!errors.username && touched.username}>
+              <Field
+                as={Input}
+                id="username"
+                name="username"
+                type="text"
+                variant="filled"
+                mb={3}
+                width={60}
+                color="customGreen.50"
+                validate={(value: string) => {
+                  if (!value) {
+                    return "Username is required";
+                  }
+                }}
+              />
+              <FormErrorMessage position="absolute" bottom={-2} >{errors.username}</FormErrorMessage>
+            </FormControl>
+            <Button
+            type="submit"
+              bg="customBlue.50"
+              color="customGreen.200"
+              mb={8}
+              width={60}
+            >
+              Log In
+            </Button>
+          </VStack>
+        </form>
+      )}
 
-        <Image
-          src="./portal-2.gif"
-          position="absolute"
-          bottom={0}
-          right={-300}
-          css={{ transform: "rotate(180deg)" }}
-          ref={rickPortalRef}
-          visibility={{ base: "hidden", md: "visible" }}
-        />
-        <Image
-          src="./rick.png"
-          position="absolute"
-          bottom={0}
-          right={-130}
-          height={300}
-          width="auto"
-          ref={rickRef}
-          visibility={{ base: "hidden", md: "visible" }}
-        />
-        <Image
-          src="./portal-2.gif"
-          position="absolute"
-          bottom={0}
-          left={-300}
-          ref={mortyPortalRef}
-          visibility={{ base: "hidden", md: "visible" }}
-        />
-        <Image
-          src="./morty.png"
-          position="absolute"
-          bottom={0}
-          left={-200}
-          height={220}
-          width="auto"
-          ref={mortyRef}
-          visibility={{ base: "hidden", md: "visible" }}
-        />
-
-        <Heading mb={6} color="customGreen.50">
-          Log In
-        </Heading>
-        <Input placeholder="Rick" type="email" variant="filled" mb={3} color="customGreen.50"/>
-        {/* <Input
+      {/* <Input
           placeholder="**********"
           type="password"
           variant="filled"
           mb={6}
         /> */}
-        <Button
-          bg="customBlue.50"
-          color="customGreen.200"
-          mb={8}
-          onClick={(e: React.MouseEvent<HTMLElement>) => loginHandler(e)}
-        >
-          Log In
-        </Button>
-        {/* <FormControl display="flex" alignItems="center">
+
+      {/* <FormControl display="flex" alignItems="center">
           <FormLabel htmlFor="dark_mode" color="#c1e26a" mb="0">
             Enable Dark Mode?
           </FormLabel>
@@ -160,8 +96,7 @@ const LoginForm = () => {
             onChange={toggleColorMode}
           />
         </FormControl> */}
-      </Flex>
-    </Flex>
+    </Formik>
   );
 };
 
