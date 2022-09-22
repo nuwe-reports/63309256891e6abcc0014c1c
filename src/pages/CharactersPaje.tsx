ee87623b9ed71559cc7062d382e3bcaeee3f4a51
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-22 15:06:54 +0200

    style characters page

--- src/pages/CharactersPaje.tsx
+++ src/pages/CharactersPaje.tsx
@@ -1,32 +1,49 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Image, VStack, HStack, Wrap, WrapItem } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import Fonts from "../Fonts";
 const CharactersPage = () => {
   const [characters, setCharacters] = useState<any[]>([]);
-  console.log("hOLA")
+  console.log("hOLA");
   useEffect(() => {
     async function fetchCharacters() {
       const response = await axios(
         "https://rickandmortyapi.com/api/character?page=1"
       );
-      console.log("🚀 ~ file: CharactersPaje.tsx ~ line 13 ~ fetchCharacters ~ response", response)
+      console.log(
+        "🚀 ~ file: CharactersPaje.tsx ~ line 13 ~ fetchCharacters ~ response",
+        response
+      );
       setCharacters(response.data.results);
     }
     fetchCharacters();
   }, []);
   return (
-    <Box bg="black" w="100vw" h="100vh">
-      <Heading mb={6} color="#c1e26a">
-        CharactersPage
+    <Box bg="customGreen.200" max-w="100vw" h="fit-content">
+      <Fonts />
+
+      <Heading
+        color="customBlue.50"
+        fontSize={{base:60, md:120}}
+        textShadow="2.5px 0 #c1e26a, -2.5px 0 #c1e26a, 0 2.5px #c1e26a, 0 -2.5px #c1e26a"
+        pt={10}
+      >
+        CHARACTERS
       </Heading>
-      {characters.map((character, index) => {
-        return (
-          <Text key={index} color="#c1e26a">
-            {character.name}
-          </Text>
-        );
-      })}
+
+      <Wrap spacing={{base:8, lg:24}} px={{base:8, xl:160}} pt={18} justify="center">
+        {characters.map((character, index) => {
+          return (
+            <WrapItem>
+            <VStack key={index}>
+              <Image src={character.image} alt={character.name + "Image"} maxW={64}/>
+              <Text color="#c1e26a">{character.name}</Text>
+            </VStack>
+            </WrapItem>
+          );
+        })}
+      </Wrap>
+
     </Box>
   );
 };
