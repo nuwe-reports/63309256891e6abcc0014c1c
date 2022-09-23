76356e5bf7393f68f2054fcb77b0625079d3cc29
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-23 18:33:40 +0200

    feat 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import './App.css'
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import CharactersPage from './pages/CharactersPage';
-
+import LoginPage from "./pages/LoginPage";
+import CharactersPage from "./pages/CharactersPage";
+import ErrorPage from "./pages/ErrorPage";
 function App() {
-
   return (
     <div className="App">
-    <Routes>
-      <Route  path="/" element={<LoginPage/>} />
-      <Route  path="/characters/:page" element={<CharactersPage/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/characters/:page" element={<CharactersPage />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
