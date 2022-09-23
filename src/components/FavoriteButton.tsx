0c77b38ee398afe183fab2d9c05ddd77052fb712
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-23 19:08:44 +0200

    feat add to favorites tooltip

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -1,28 +1,29 @@
 import { StarIcon } from "@chakra-ui/icons";
+import { Tooltip } from "@chakra-ui/react";
 
 interface Props {
-    favorites: number[];
-    id:number;
-    handleFavoriteClick: (e: React.MouseEvent<SVGElement>, id: number) => void;
+  favorites: number[];
+  id: number;
+  handleFavoriteClick: (e: React.MouseEvent<SVGElement>, id: number) => void;
 }
 
-const FavoriteButton = ({favorites, id, handleFavoriteClick}:Props) => {
-    return(
-        <>
-        {favorites.includes(id) ? (
-            <StarIcon
-              color="customBlue.50"
-              onClick={(e) => handleFavoriteClick(e, id)}
-            />
-          ) : (
-            <StarIcon
-              color="transparent"
-              stroke="customGreen.50"
-              onClick={(e) => handleFavoriteClick(e,id)}
-            />
-          )}
-          </>
-    )
-}
+const FavoriteButton = ({ favorites, id, handleFavoriteClick }: Props) => {
+  return (
+    <Tooltip hasArrow label="Add to favorites" bg="customBlue.50" color="customGreen.200" border="1px" borderColor="customGreen.50">
+      {favorites.includes(id) ? (
+        <StarIcon
+          color="customBlue.50"
+          onClick={(e) => handleFavoriteClick(e, id)}
+        />
+      ) : (
+        <StarIcon
+          color="transparent"
+          stroke="customGreen.50"
+          onClick={(e) => handleFavoriteClick(e, id)}
+        />
+      )}
+    </Tooltip>
+  );
+};
 
-export default FavoriteButton
+export default FavoriteButton;
