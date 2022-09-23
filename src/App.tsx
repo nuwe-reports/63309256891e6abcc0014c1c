23c9b9466e037da215a13cbccdef82d19fba2777
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-23 23:10:16 +0200

    fix private route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CharactersPage from "./pages/CharactersPage";
 import ErrorPage from "./pages/ErrorPage";
+import PrivateRoute from "./components/routes/PrivateRoute";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/characters/:page" element={<CharactersPage />} />
+        <Route path="/characters/:page" element={<PrivateRoute><CharactersPage /></PrivateRoute>} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
