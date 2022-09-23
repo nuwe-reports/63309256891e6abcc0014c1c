f7cbbc2fe67562780065a8b991409b2e16c6ae71
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-23 11:37:39 +0200

    style character modal

--- src/components/CharacterModal.tsx
+++ src/components/CharacterModal.tsx
@@ -12,6 +12,7 @@ import {
   HStack,
   Box,
   Heading,
+  Circle,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -40,7 +41,7 @@ function CharacterModal({ isOpen, onOpen, onClose, id }: Props) {
   }, []);
   return (
     <>
-      <Modal onClose={onClose} isOpen={isOpen} isCentered size="xl" >
+      <Modal onClose={onClose} isOpen={isOpen} isCentered size="xl">
         <ModalOverlay
           bg="blackAlpha.300"
           backdropFilter="blur(10px) hue-rotate(90deg)"
@@ -50,11 +51,10 @@ function CharacterModal({ isOpen, onOpen, onClose, id }: Props) {
           color="customGreen.50"
           border="2px"
           borderColor="customGreen.50"
-          
         >
           <ModalCloseButton />
           <ModalBody pt={12} px={12}>
-            <HStack align="flex-start">
+            <HStack>
               <Image
                 src={character.image}
                 alt={character.name + "Image"}
@@ -62,18 +62,50 @@ function CharacterModal({ isOpen, onOpen, onClose, id }: Props) {
                 borderRadius={4}
               />
               <Box pl={4}>
-                <ModalHeader pt={0} pl={0}>{character.name}</ModalHeader>
-                <Text>
-                  {character.status} {character.gender} {character.species}
-                </Text>
-                <Text>{character.origin?.name}</Text>
+                <ModalHeader pt={0} pl={0}>
+                  {character.name}
+                </ModalHeader>
+                <HStack>
+                  {character.status === "Alive" ? (
+                    <Circle size="10px" bg="green.400" />
+                  ) : character.status === "Dead" ? (
+                    <Circle size="10px" bg="red" />
+                  ) : (
+                    <Circle size="10px" bg="yellow.500" />
+                  )}
 
-                <Text>{character.location?.name}</Text>
+                  <Text>
+                    {character.status} - {character.gender} -{" "}
+                    {character.species}
+                  </Text>
+                </HStack>
+                <Box pt={4}>
+                  <Text color="gray.400">Last known location:</Text>
+
+                  <Text>{character.origin?.name}</Text>
+                </Box>
+                <Box pt={4}>
+
+                  <Text color="gray.400">First seen in:</Text>
+
+                  <Text>{character.location?.name}</Text>
+                </Box>
               </Box>
             </HStack>
           </ModalBody>
           <ModalFooter pt={0}>
-            <Button color="customGreen.100" bg="customGreen.50" _hover={{ bg: "customGreen.100", borderColor: "customGreen.50", color:"customGreen.50"}} onClick={onClose}>Close</Button>
+            <Button
+              color="customGreen.100"
+              bg="customGreen.50"
+              _hover={{
+                bg: "customGreen.100",
+                borderColor: "customGreen.50",
+                color: "customGreen.50",
+              }}
+              onClick={onClose}
+            >
+              Close
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
