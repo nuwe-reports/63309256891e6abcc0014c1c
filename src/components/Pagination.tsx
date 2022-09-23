9755dde6233e48881c3960d7cc4b4dbdd60a2da6
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-23 15:30:28 +0200

    feat pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,107 @@
+import React, { FC, ChangeEvent, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import {
+  Pagination,
+  usePagination,
+  PaginationNext,
+  PaginationPage,
+  PaginationPrevious,
+  PaginationContainer,
+  PaginationPageGroup,
+  PaginationSeparator,
+} from "@ajna/pagination";
+
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+
+interface Props {
+  totalPages: number;
+}
+
+const PageButtons = ({ totalPages }: Props) => {
+  const { currentPage, setCurrentPage, pagesCount, pages } = usePagination({
+    pagesCount: totalPages,
+    initialState: { currentPage: 1 },
+    limits: {
+      outer: 1,
+      inner: 3,
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const handlePageChange = (nextPage: number): void => {
+    setCurrentPage(nextPage);
+    navigate(`/characters/${nextPage}`);
+
+    console.log("request new data with ->", nextPage);
+  };
+
+  return (
+    <Pagination
+      pagesCount={pagesCount}
+      currentPage={currentPage}
+      onPageChange={handlePageChange}
+    >
+      <PaginationContainer align="center" justify="center" p={4} w="full">
+        <PaginationPrevious
+          p={0}
+          mr={2}
+          bg="transparent"
+          _hover={{
+            bg: "customBlue.50",
+            borderColor: "customGreen.50",
+          }}
+        >
+          <ChevronLeftIcon color="customGreen.50" w={6} h={6} />
+        </PaginationPrevious>
+        <PaginationPageGroup
+          separator={
+            <PaginationSeparator
+              color="customGreen.50"
+              fontSize="sm"
+              w={7}
+              jumpSize={11}
+              disabled
+              pointerEvents="none"
+            />
+          }
+        >
+          {pages.map((page: number) => (
+            <PaginationPage
+              key={`pagination_page_${page}`}
+              page={page}
+              bg="customGreen.100"
+              color="customGreen.50"
+              border="1px"
+              borderColor="customGreen.50"
+              _hover={{
+                bg: "customBlue.50",
+                color: "customGreen.100",
+                borderColor: "customGreen.50",
+              }}
+              _current={{
+                bg: "customBlue.50",
+                color: "customGreen.100",
+                borderColor: "customGreen.50",
+              }}
+            />
+          ))}
+        </PaginationPageGroup>
+        <PaginationNext
+          p={0}
+          ml={2}
+          bg="transparent"
+          _hover={{
+            bg: "customBlue.50",
+            borderColor: "customGreen.50",
+          }}
+        >
+          <ChevronRightIcon color="customGreen.50" w={6} h={6} />
+        </PaginationNext>
+      </PaginationContainer>
+    </Pagination>
+  );
+};
+
+export default PageButtons;
--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -4,22 +4,12 @@ import {
   Text,
   Image,
   VStack,
-  HStack,
   Wrap,
   WrapItem,
   useDisclosure,
 } from "@chakra-ui/react";
+import PageButtons from "../components/Pagination";
 
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  Button,
-} from '@chakra-ui/react'
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Fonts from "../Fonts";
@@ -27,28 +17,35 @@ import { useParams } from "react-router-dom";
 import CharacterModal from "../components/CharacterModal";
 const CharactersPage = () => {
   const [characters, setCharacters] = useState<any[]>([]);
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [id, setId] = useState<number|null>(null)
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [id, setId] = useState<number | null>(null);
   const { page } = useParams();
 
-const handleCLick = (id:number) =>{
-  setId(id)
-  onOpen()
-}
+  const handleCLick = (id: number) => {
+    setId(id);
+    onOpen();
+  };
 
   useEffect(() => {
     async function fetchCharacters() {
       const response = await axios(
-        `https://rickandmortyapi.com/api/character?page=${page}` 
+        `https://rickandmortyapi.com/api/character?page=${page}`
       );
       setCharacters(response.data.results);
+      setTotalPages(response.data.info.pages);
     }
     fetchCharacters();
-  }, []);
-  return (
-    <Box bg="customGreen.200" max-w="100vw" h="fit-content">
-      <Fonts />
+  }, [page]);
 
+  return (
+    <Box
+      bg="customGreen.200"
+      max-w="100vw"
+      h="fit-content"
+      px={{ base: 8, xl: 160 }}
+    >
+      <Fonts />
       <Heading
         color="customBlue.50"
         fontSize={{ base: 60, md: 120 }}
@@ -57,13 +54,9 @@ const handleCLick = (id:number) =>{
       >
         CHARACTERS
       </Heading>
+      <PageButtons totalPages={totalPages}></PageButtons>
 
-      <Wrap
-        spacing={{ base: 8, lg: 20 }}
-        px={{ base: 8, xl: 160 }}
-        pt={18}
-        justify="center"
-      >
+      <Wrap spacing={{ base: 8, lg: 20 }} pt={18} justify="center">
         {characters.map((character, index) => {
           return (
             <WrapItem
@@ -72,8 +65,8 @@ const handleCLick = (id:number) =>{
               bg="customGreen.100"
               p={4}
               onClick={() => handleCLick(character.id)}
-              cursor={'pointer'}
-              _hover={{ opacity:0.8}}
+              cursor={"pointer"}
+              _hover={{ opacity: 0.8 }}
             >
               <VStack key={index}>
                 <Image
@@ -81,15 +74,26 @@ const handleCLick = (id:number) =>{
                   alt={character.name + "Image"}
                   maxW={64}
                 />
-                <Text color="customGreen.50">{character.name.toUpperCase()}</Text>
+                <Text color="customGreen.50">
+                  {character.name.toUpperCase()}
+                </Text>
               </VStack>
             </WrapItem>
           );
         })}
       </Wrap>
-      {isOpen && <CharacterModal isOpen={isOpen} onOpen = {onOpen} onClose = {onClose} id={id}/>}
-      
+      {isOpen && (
+        <CharacterModal
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onClose={onClose}
+          id={id}
+        />
+      )}
+      <PageButtons totalPages={totalPages}></PageButtons>
+
     </Box>
+
   );
 };
 
