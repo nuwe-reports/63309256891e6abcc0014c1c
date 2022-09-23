55e29a921cd9630235dfa9407638e58c1815df94
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-23 17:54:56 +0200

    clean App unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import CharactersPage from './pages/CharactersPage';
-import PrivateRoute from './components/routes/PrivateRoute'; 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
