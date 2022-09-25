3e5ec080ca76fc32e851a7dcc159a622afd332c6
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-25 19:22:11 +0200

    style favorite page text responsive size

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,10 +1,19 @@
-import { Box, Image, Wrap, useDisclosure, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Wrap,
+  useDisclosure,
+  VStack,
+  Text,
+} from "@chakra-ui/react";
 import { useState, useContext } from "react";
 import CharacterModal from "../components/CharacterModal";
 import CharacterCard from "../components/CharacterCard";
 import { Character, FavoriteContext } from "../types";
 import Header from "../components/Header";
 import { FavoritesContext } from "../context/favorites.context";
+import { StarIcon } from "@chakra-ui/icons";
+
 const INITIAL_STATE = {
   created: "",
   episode: [""],
@@ -49,17 +58,35 @@ const FavoritesPage = () => {
           pt={10}
           filter="drop-shadow(2px 0 0 #c1e26a) drop-shadow(0 2px 0 #c1e26a) drop-shadow(-2px 0 0 #c1e26a) drop-shadow(0 2px 0 #c1e26a)  "
         />
-        <Wrap spacing={{ base: 8, lg: 20 }} pt={18} justify="center">
-          {favorites.map((character: Character, index: number) => {
-            return (
-              <CharacterCard
-                key={index}
-                character={character}
-                handleCardCLick={handleCardCLick}
-              />
-            );
-          })}
-        </Wrap>
+        {favorites.length ? (
+          <Wrap spacing={{ base: 8, lg: 20 }} pt={18} justify="center">
+            {favorites.map((character: Character, index: number) => {
+              return (
+                <CharacterCard
+                  key={index}
+                  character={character}
+                  handleCardCLick={handleCardCLick}
+                />
+              );
+            })}
+          </Wrap>
+        ) : (
+          <Box>
+            <Text
+              color="customGreen.50"
+              pt={12}
+              fontSize={{base:"4xl", md:"7xl"}}
+              fontWeight="bold"
+            >
+              No favorites yet
+            </Text>{" "}
+            <Text color="customGreen.50" fontSize={{base:"md", md:"xl"}}>
+              Add your favorites by clicking the{" "}
+              <StarIcon color="transparent" stroke="customGreen.50" mb={1}/> icon
+            </Text>
+          </Box>
+        )}
+
         {isOpen && (
           <CharacterModal
             isOpen={isOpen}
